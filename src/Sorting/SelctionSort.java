7d989e368c0f6ca286255125e6c2f277313a02a3
Jayaji1 <jayavishwakarma846@gmail.com>
2025-04-27 14:29:06 +0530

    first commit

---
+++ src/Sorting/SelctionSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class SelctionSort {
+    public static void main(String[] args) {
+        int a[]={35,89,20,30,45};
+        int min,temp=0;
+        for(int i=0;i<a.length;i++){
+            min=i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]<a[min]){
+                    min=j;
+                }
+            }
+            temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
